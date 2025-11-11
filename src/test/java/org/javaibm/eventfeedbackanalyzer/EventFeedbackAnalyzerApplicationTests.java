7c60d9ccd3c111acbee2e64847d03296e88eff96
Konstantin <konstantin.kernazhytski@gmail.com>
2025-11-11 19:01:51 +0200

    setup

---
+++ src/test/java/org/javaibm/eventfeedbackanalyzer/EventFeedbackAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaibm.eventfeedbackanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFeedbackAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
